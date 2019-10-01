66c26baeecbfc88e8a167a2cf21f80164f7ed70e
Brad Farberman <bfarberman@gmail.com>
2019-10-01 14:38:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,15 +23,10 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 
+def 
 
-def reduce_to_all_true(source_array)
-    counter = 0 
-    while counter < array.size.do 
-    return false if !source_array[counter] 
-    counter += 1 
-  end
-  return true 
-end 
+
+
 
  def reduce_to_all_true(source_array)
     source_array.reduce {}
